6889b1a24defe71f3af9f10adaff7bdd9b9d5fff
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-01-13 08:36:16 +0200

    Initial commit

---
+++ HRLeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Domain
+{
+    public class LeaveAllocation
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+        public DateTime DateCreated { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
