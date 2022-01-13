2cb49b0137ba2a0041a4c003cf2110ae573f0a2f
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-01-13 09:26:45 +0200

    Added ApplicationServicesRegistrator.cs for registering services though depencency injection when a client calls a method.

---
+++ HRLeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,29 @@
+﻿using HRLeaveManagement.Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application
+{
+    // For Dependency injection.
+    // When client calls a method, services will be registered
+    public static class ApplicationServicesRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            // Another way:
+            // Registers MappingProfile as the AutoMapper configuration.
+            // But for every mapping profile, this has to be repeated.
+            // services.AddAutoMapper(typeof(MappingProfile));
+
+            // Better way:
+            // Will traverse through every mapping profile that has its inheritance
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
