2111afd3aed6b8a72fcad5008f0f8b4606393859
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-01-13 08:52:36 +0200

    Added Persistance interfaces for specific classes

---
+++ HRLeaveManagement.Application/Persistance/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,14 @@
+﻿using HRLeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Persistance.Contracts
+{
+    public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+    {
+
+    }
+}
