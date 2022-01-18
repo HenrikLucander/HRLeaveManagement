3e51617b5252f7b950a2a442be2acab656b3fda3
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-01-18 15:13:07 +0200

    Added migration SeedingLeaveTypes

---
+++ HR.LeaveManagement.Persistance/Configurations/Entities/LeaveRequestConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using HRLeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistance.Configurations.Entities
+{
+    public class LeaveRequestConfiguration : IEntityTypeConfiguration<LeaveAllocation>
+    {
+        public void Configure(EntityTypeBuilder<LeaveAllocation> builder)
+        {
+
+        }
+    }
+}
--- HRLeaveManagement.Domain/Common/BaseDomainEntity.cs
+++ HRLeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -12,8 +12,8 @@ namespace HRLeaveManagement.Domain.Common
     {
         public int Id { get; set; }
         public DateTime DateCreated { get; set; }
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
         public DateTime LastModifiedDate { get; set; }
-        public string LastModifiedBy { get; set; }
+        public string? LastModifiedBy { get; set; }
     }
 }
