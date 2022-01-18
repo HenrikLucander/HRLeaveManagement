b919e5cd60dd21efd7628e235dacc1edd7405824
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-01-18 09:16:52 +0200

    Added DbContext.

---
+++ HR.LeaveManagement.Persistance/LeaveManagementDbContext.cs
@@ -0,0 +1,48 @@
+﻿using HRLeaveManagement.Domain;
+using HRLeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistance
+{
+    public class LeaveManagementDbContext : DbContext
+    {
+        public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+        }
+
+        /// <summary>
+        /// Basic auditing code. Will call base SaveChangesAsync after setting LastModifiedDate and DateCreated.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+    }
+}
