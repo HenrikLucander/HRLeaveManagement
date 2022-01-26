de4e4e7cdee8fc77889ba6fce1009191bd4969de
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-01-26 07:46:29 +0200

    Added IdentityServicesRegistration for dependency injection

--- HR.LeaveManagement.Identity/IdentityServicesRegistration.cs
+++ HR.LeaveManagement.Identity/IdentityServicesRegistration.cs
@@ -1,13 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Application.Models.Identity;
+using HR.LeaveManagement.Identity.Models;
+using HR.LeaveManagement.Identity.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Identity
 {
-    public class IdentityServicesRegistration
+    public static class IdentityServicesRegistration
     {
+        public static IServiceCollection ConfigureIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
 
+            services.AddDbContext<LeaveManagementIdentityDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("LeaveManagementIdentityConnectionString"),
+                b => b.MigrationsAssembly(typeof(LeaveManagementIdentityDbContext).Assembly.FullName)));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<LeaveManagementIdentityDbContext>().AddDefaultTokenProviders();
+
+            services.AddTransient<IAuthService, AuthService>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(o =>
+                {
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = configuration["JwtSettings:Issuer"],
+                        ValidAudience = configuration["JwtSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]))
+                    };
+                });
+
+            return services;
+        }
     }
 }
